a57faa5c8151146e6d8d234f3d74f61d9ff49475
Virginia Toledo Gavagnin <118676564+VirginiaXiao148@users.noreply.github.com>
2025-11-27 13:28:52 +0100

    feat: add Subject entity with attributes and enumerations for type and status

---
+++ backend/src/main/java/com/example/backend/model/Subject.java
@@ -0,0 +1,40 @@
+package com.example.backend.model;
+
+import java.math.BigDecimal;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Subject {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private int credits;
+    @Enumerated(EnumType.STRING)
+    private SubjectType type; // Basic, Mandatory or Optional
+    private int year; // 1, 2, 3 or 4
+    private BigDecimal note;
+    @Enumerated(EnumType.STRING)
+    private SubjectStatus status; // Passed, Failed or Matriculated
+
+    enum SubjectType {
+        BASIC,
+        MANDATORY,
+        OPTIONAL
+    }
+
+    enum SubjectStatus {
+        PASSED,
+        FAILED,
+        MATRICULATED
+    }
+}
